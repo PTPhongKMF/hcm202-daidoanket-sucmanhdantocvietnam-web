8f78018ae1ad8334e2127b2b668d2c7b297849e5
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-09-19 00:02:50 +0700

    feat: footer and clean up

--- src/components/Home/Part1.tsx
+++ src/components/Home/Part1.tsx
@@ -1,31 +1,6 @@
-import { ChevronDownIcon } from "lucide-react";
 
 export default function Part1() {
   return (
-    <>
-      <div className="h-[50rem] bg-[url(/imgs/part1-1.jpg)] bg-cover">
-        <div className="size-full bg-yellow-800/50 grid grid-rows-[1fr_5rem]">
-          <div className="flex flex-col gap-6 justify-center items-center
-         text-gray-200 text-center text-6xl font-title px-80">
-            <h2 className="drop-shadow-lg drop-shadow-black">ĐẠI ĐOÀN KẾT</h2>
-            <p className="drop-shadow-lg drop-shadow-black font-sans">-</p>
-            <h2 className="drop-shadow-lg drop-shadow-black">Sức mạnh dân tộc Việt Nam</h2>
-
-            <em className="flex font-sans justify-center items-center w-full text-white text-2xl text-center mt-20 drop-shadow-lg drop-shadow-black">
-              “Kết nối truyền thống – Ứng dụng công nghệ – Lan tỏa giá trị đoàn kết”
-            </em>
-          </div>
-
-
-        </div>
-
-
-      </div>
-      <div className="flex flex-col justify-center items-center bg-linear-to-br from *:">
-        <ChevronDownIcon className="size-8 animate-arrowFlow text-black font-bold" />
-        <ChevronDownIcon className="size-8 animate-arrowFlow text-black font-bold" />
-        <ChevronDownIcon className="size-8 animate-arrowFlow text-black font-bold" />
-      </div>
-    </>
+    <div>part 1</div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,12 @@ import Part2 from "../components/Home/Part2";
 import Part3 from "../components/Home/Part3";
 import Part4 from "../components/Home/Part4";
 import Part5 from "../components/Home/Part5";
+import Title from "../components/Home/Title";
 
 export default function Home() {
   return (
     <>
+      <Title />
       <Part1 />
       <Part2 />
       <Part3 />
